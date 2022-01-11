9c9c4ee1bd03aa28a21612064d2ffbd9022b1bb9
MatthiasBienvenu <matthiasbienvenu@outlook.fr>
2022-01-11 19:29:00 +0100

    Update monopoly

--- monopoly.py
+++ monopoly.py
@@ -183,14 +183,12 @@ MEDITERRANEAN_AVENUE = Property('MEDITERRANEAN_AVENUE', 1, brown, [60, 50], {0: 
 BALTIC_AVENUE = Property('BALTIC_AVENUE', 1, brown, [60, 50], {0: 4, 1: 20, 2: 60, 3: 180, 4: 320, 5: 450})
 ORIENTAL_AVENUE = Property('ORIENTAL_AVENUE', 1, skyblue, [100, 50], {0: 6, 1: 30, 2: 90, 3: 270, 4: 400, 5: 550})
 VERMONT_AVENUE = Property('VERMONT_AVENUE', 1, skyblue, [100, 50], {0: 6, 1: 30, 2: 90, 3: 270, 4: 400, 5: 550})
-CONNECTICUT_AVENUE = Property('CONNECTICUT_AVENUE', 1, skyblue, [120, 50],
-                              {0: 8, 1: 40, 2: 100, 3: 300, 4: 450, 5: 600})
+CONNECTICUT_AVENUE = Property('CONNECTICUT_AVENUE', 1, skyblue, [120, 50], {0: 8, 1: 40, 2: 100, 3: 300, 4: 450, 5: 600})
 ST_CHARLES_PLACE = Property('ST_CHARLES_PLACE', 1, pink, [140, 100], {0: 10, 1: 50, 2: 150, 3: 450, 4: 625, 5: 750})
 STATES_AVENUE = Property('STATES_AVENUE', 1, pink, [140, 100], {0: 10, 1: 50, 2: 150, 3: 450, 4: 625, 5: 750})
 VIRGINIA_AVENUE = Property('VIRGINIA_AVENUE', 1, pink, [160, 100], {0: 12, 1: 60, 2: 180, 3: 500, 4: 700, 5: 900})
 ST_JAMES_PLACE = Property('ST_JAMES_PLACE', 1, orange, [180, 100], {0: 14, 1: 70, 2: 200, 3: 550, 4: 700, 5: 900})
-TENNESSEE_AVENUE = Property('TENNESSEE_AVENUE', 1, orange, [180, 100],
-                            {0: 14, 1: 70, 2: 200, 3: 550, 4: 700, 5: 950})  # 950 vs 900 ???
+TENNESSEE_AVENUE = Property('TENNESSEE_AVENUE', 1, orange, [180, 100],{0: 14, 1: 70, 2: 200, 3: 550, 4: 700, 5: 950})  # 950 vs 900 ???
 NEW_YORK_AVENUE = Property('NEW_YORK_AVENUE', 1, orange, [200, 100], {0: 16, 1: 80, 2: 220, 3: 600, 4: 800, 5: 1000})
 KENTUCKY_AVENUE = Property('KENTUCKY_AVENUE', 1, red, [220, 150], {0: 18, 1: 90, 2: 250, 3: 700, 4: 875, 5: 1050})
 INDIANA_AVENUE = Property('INDIANA_AVENUE', 1, red, [220, 150], {0: 18, 1: 90, 2: 250, 3: 700, 4: 875, 5: 1050})
@@ -199,15 +197,11 @@ ATLANTIC_AVENUE = Property('ATLANTIC_AVENUE', 1, yellow, [260, 150], {0: 22, 1: 
 VENTNOR_AVENUE = Property('VENTNOR_AVENUE', 1, yellow, [260, 150], {0: 22, 1: 110, 2: 330, 3: 800, 4: 975, 5: 1150})
 MARVIN_GARDENS = Property('MARVIN_GARDENS', 1, yellow, [280, 150], {0: 24, 1: 120, 2: 360, 3: 850, 4: 1025, 5: 1200})
 PACIFIC_AVENUE = Property('PACIFIC_AVENUE', 1, green, [300, 200], {0: 26, 1: 130, 2: 390, 3: 900, 4: 1100, 5: 1275})
-NORTH_CAROLINA_AVENUE = Property('NORTH_CAROLINA_AVENUE', 1, green, [300, 200],
-                                 {0: 26, 1: 130, 2: 390, 3: 900, 4: 1100, 5: 1275})
-PENNSYLVANIA_AVENUE = Property('PENNSYLVANIA_AVENUE', 1, green, [320, 200],
-                               {0: 28, 1: 150, 2: 450, 3: 1000, 4: 1200, 5: 1400})
+NORTH_CAROLINA_AVENUE = Property('NORTH_CAROLINA_AVENUE', 1, green, [300, 200], {0: 26, 1: 130, 2: 390, 3: 900, 4: 1100, 5: 1275})
+PENNSYLVANIA_AVENUE = Property('PENNSYLVANIA_AVENUE', 1, green, [320, 200], {0: 28, 1: 150, 2: 450, 3: 1000, 4: 1200, 5: 1400})
 PARK_PLACE = Property('PARK_PLACE', 1, darkblue, [350, 200], {0: 35, 1: 175, 2: 500, 3: 1100, 4: 1300, 5: 1500})
 BOARDWALK = Property('BOARDWALK', 1, darkblue, [400, 200], {0: 50, 1: 200, 2: 600, 3: 1400, 4: 1700, 5: 2000})
 
-Lcases = [a]
+Lcases = []
 
 p = Player('player')
-
-a.action(p)
