d9e95a6f9112687ae17c3e03a956833326c0377f
MatthiasBienvenu <matthiasbienvenu@outlook.fr>
2022-01-12 20:38:10 +0100

    Update monopoly

--- monopoly.py
+++ monopoly.py
@@ -3,7 +3,7 @@ from random import randint
 
 
 class Player:
-    def __init__(self, name):
+    def __init__(self, name: str):
         self.name = name
         self.dobble = 0
         self.balance = 1500
@@ -35,24 +35,29 @@ class Player:
             self.dobble = 0
 
 
+Lcases = []
+
+
 class Box:
-    def __init__(self, name, pos):
+    def __init__(self, name: str, pos: int):
+        global Lcases
         self.name = name
         self.pos = pos
+        Lcases.append(self)
 
 
 class Property(Box):
-    def __init__(self, name, pos, group, price, rentDict):
-        # name = str ; pos = int 0<=pos<=40 ; group = [Property...] ; price = [priceCase, priceHouse] ;
-        # rentDict = dict
+    def __init__(self, name: str, pos: int, group: list, price: list, rent: dict):
+        # pos = int 0<=pos<=40 ; price = [priceCase, priceHouse]
         super().__init__(name, pos)
         self.houses = 0
         self.group = group
         self.price = price
-        self.rentDict = rentDict
+        self.rent = rent
         self.owner = None
+        self.group.append(self)
 
-    def action(self, player):
+    def action(self, player: Player) -> None:
         if self.owner is None:
             # !!! choice
             if player.balance >= self.price[0]:
@@ -83,38 +88,38 @@ class Property(Box):
                         print('wrong input')
         else:
             # !!! faillite
-            price = self.rentDict[self.houses]
+            price = self.rent[self.houses]
             player.balance -= price
             self.owner.balance += price
 
 
 class Special(Box):
-    def __init__(self, name, pos, action):  # name = str ; pos = int 0<=pos<=40 ; action = function
+    def __init__(self, name: str, pos: int, action: callable):  # pos = int 0<=pos<=40
         super().__init__(name, pos)
         self.action = action
 
 
 # ----- functions for special boxes -----
 
-def go(player):
+def go(player: Player) -> None:
     pass
 
 
-def chance(player):
+def chance(player: Player) -> None:
     card = Lchance[0]
     card(player)
     del (Lchance[0])
     Lchance.append(card)
 
 
-def community_chest(player):
+def community_chest(player: Player) -> None:
     card = Lcomchest[0]
     card(player)
     del (Lcomchest[0])
     Lcomchest.append(card)
 
 
-def go_to_jail(player):
+def go_to_jail(player: Player) -> None:
     player.pos = 10
     player.location = Lcases[10]
     player.jailcount = 1
@@ -123,25 +128,25 @@ def go_to_jail(player):
 fees = 0
 
 
-def tax100(player):
+def tax100(player: Player) -> None:
     global fees
     player.balance -= 100  # !!! faillite
     fees += 100
 
 
-def tax200(player):
+def tax200(player: Player) -> None:
     global fees
     player.balance -= 200  # !!! faillite
     fees += 200
 
 
-def free_parking(player):
+def free_parking(player: Player) -> None:
     global fees
     player.balance += fees
     fees = 0
 
 
-def jail(player):
+def jail(player: Player) -> None:
     if player.jailcount != 0:
         dice1 = randint(1, 6)
         dice2 = randint(1, 6)
@@ -166,7 +171,6 @@ Lcomchest = []
 # ----- List of the boxes -----
 
 
-GO = Special('GO', 0, go)
 
 # prop = Property('name', 0, [], [4000], {})
 
@@ -180,29 +184,47 @@ yellow = []
 green = []
 darkblue = []
 
+GO = Special('GO', 0, go)
 MEDITERRANEAN_AVENUE = Property('MEDITERRANEAN_AVENUE', 1, brown, [60, 50], {0: 2, 1: 10, 2: 30, 3: 90, 4: 160, 5: 250})
-BALTIC_AVENUE = Property('BALTIC_AVENUE', 1, brown, [60, 50], {0: 4, 1: 20, 2: 60, 3: 180, 4: 320, 5: 450})
-ORIENTAL_AVENUE = Property('ORIENTAL_AVENUE', 1, skyblue, [100, 50], {0: 6, 1: 30, 2: 90, 3: 270, 4: 400, 5: 550})
-VERMONT_AVENUE = Property('VERMONT_AVENUE', 1, skyblue, [100, 50], {0: 6, 1: 30, 2: 90, 3: 270, 4: 400, 5: 550})
-CONNECTICUT_AVENUE = Property('CONNECTICUT_AVENUE', 1, skyblue, [120, 50], {0: 8, 1: 40, 2: 100, 3: 300, 4: 450, 5: 600})
-ST_CHARLES_PLACE = Property('ST_CHARLES_PLACE', 1, pink, [140, 100], {0: 10, 1: 50, 2: 150, 3: 450, 4: 625, 5: 750})
-STATES_AVENUE = Property('STATES_AVENUE', 1, pink, [140, 100], {0: 10, 1: 50, 2: 150, 3: 450, 4: 625, 5: 750})
-VIRGINIA_AVENUE = Property('VIRGINIA_AVENUE', 1, pink, [160, 100], {0: 12, 1: 60, 2: 180, 3: 500, 4: 700, 5: 900})
-ST_JAMES_PLACE = Property('ST_JAMES_PLACE', 1, orange, [180, 100], {0: 14, 1: 70, 2: 200, 3: 550, 4: 700, 5: 900})
-TENNESSEE_AVENUE = Property('TENNESSEE_AVENUE', 1, orange, [180, 100],{0: 14, 1: 70, 2: 200, 3: 550, 4: 700, 5: 950})  # 950 vs 900 ???
-NEW_YORK_AVENUE = Property('NEW_YORK_AVENUE', 1, orange, [200, 100], {0: 16, 1: 80, 2: 220, 3: 600, 4: 800, 5: 1000})
-KENTUCKY_AVENUE = Property('KENTUCKY_AVENUE', 1, red, [220, 150], {0: 18, 1: 90, 2: 250, 3: 700, 4: 875, 5: 1050})
-INDIANA_AVENUE = Property('INDIANA_AVENUE', 1, red, [220, 150], {0: 18, 1: 90, 2: 250, 3: 700, 4: 875, 5: 1050})
-ILLINOIS_AVENUE = Property('ILLINOIS_AVENUE', 1, red, [240, 150], {0: 20, 1: 100, 2: 300, 3: 750, 4: 925, 5: 1100})
-ATLANTIC_AVENUE = Property('ATLANTIC_AVENUE', 1, yellow, [260, 150], {0: 22, 1: 110, 2: 330, 3: 800, 4: 975, 5: 1150})
-VENTNOR_AVENUE = Property('VENTNOR_AVENUE', 1, yellow, [260, 150], {0: 22, 1: 110, 2: 330, 3: 800, 4: 975, 5: 1150})
-MARVIN_GARDENS = Property('MARVIN_GARDENS', 1, yellow, [280, 150], {0: 24, 1: 120, 2: 360, 3: 850, 4: 1025, 5: 1200})
-PACIFIC_AVENUE = Property('PACIFIC_AVENUE', 1, green, [300, 200], {0: 26, 1: 130, 2: 390, 3: 900, 4: 1100, 5: 1275})
-NORTH_CAROLINA_AVENUE = Property('NORTH_CAROLINA_AVENUE', 1, green, [300, 200], {0: 26, 1: 130, 2: 390, 3: 900, 4: 1100, 5: 1275})
-PENNSYLVANIA_AVENUE = Property('PENNSYLVANIA_AVENUE', 1, green, [320, 200], {0: 28, 1: 150, 2: 450, 3: 1000, 4: 1200, 5: 1400})
-PARK_PLACE = Property('PARK_PLACE', 1, darkblue, [350, 200], {0: 35, 1: 175, 2: 500, 3: 1100, 4: 1300, 5: 1500})
-BOARDWALK = Property('BOARDWALK', 1, darkblue, [400, 200], {0: 50, 1: 200, 2: 600, 3: 1400, 4: 1700, 5: 2000})
+COMMUNITY_CHEST1 = Special('COMMUNITY_CHEST', 2, community_chest)
+BALTIC_AVENUE = Property('BALTIC_AVENUE', 3, brown, [60, 50], {0: 4, 1: 20, 2: 60, 3: 180, 4: 320, 5: 450})
+INCOME_TAX = Special('INCOME_TAX', 4, tax200)
+# gare 1
+ORIENTAL_AVENUE = Property('ORIENTAL_AVENUE', 6, skyblue, [100, 50], {0: 6, 1: 30, 2: 90, 3: 270, 4: 400, 5: 550})
+CHANCE1 = Special('CHANCE', 7, chance)
+VERMONT_AVENUE = Property('VERMONT_AVENUE', 8, skyblue, [100, 50], {0: 6, 1: 30, 2: 90, 3: 270, 4: 400, 5: 550})
+CONNECTICUT_AVENUE = Property('CONNECTICUT_AVENUE', 9, skyblue, [120, 50], {0: 8, 1: 40, 2: 100, 3: 300, 4: 450, 5: 600})
+JAIL = Special('JAIL', 10, jail)
+ST_CHARLES_PLACE = Property('ST_CHARLES_PLACE', 11, pink, [140, 100], {0: 10, 1: 50, 2: 150, 3: 450, 4: 625, 5: 750})
+# electric company
+STATES_AVENUE = Property('STATES_AVENUE', 13, pink, [140, 100], {0: 10, 1: 50, 2: 150, 3: 450, 4: 625, 5: 750})
+VIRGINIA_AVENUE = Property('VIRGINIA_AVENUE', 14, pink, [160, 100], {0: 12, 1: 60, 2: 180, 3: 500, 4: 700, 5: 900})
+# gare 2
+ST_JAMES_PLACE = Property('ST_JAMES_PLACE', 16, orange, [180, 100], {0: 14, 1: 70, 2: 200, 3: 550, 4: 750, 5: 950})
+COMMUNITY_CHEST2 = Special('COMMUNITY_CHEST', 17, community_chest)
+TENNESSEE_AVENUE = Property('TENNESSEE_AVENUE', 18, orange, [180, 100],{0: 14, 1: 70, 2: 200, 3: 550, 4: 750, 5: 950})
+NEW_YORK_AVENUE = Property('NEW_YORK_AVENUE', 19, orange, [200, 100], {0: 16, 1: 80, 2: 220, 3: 600, 4: 800, 5: 1000})
+FREE_PARKING = Special('FREE_PARKING', 20, free_parking)
+KENTUCKY_AVENUE = Property('KENTUCKY_AVENUE', 21, red, [220, 150], {0: 18, 1: 90, 2: 250, 3: 700, 4: 875, 5: 1050})
+CHANCE2 = Special('CHANCE', 22, chance)
+INDIANA_AVENUE = Property('INDIANA_AVENUE', 23, red, [220, 150], {0: 18, 1: 90, 2: 250, 3: 700, 4: 875, 5: 1050})
+ILLINOIS_AVENUE = Property('ILLINOIS_AVENUE', 24, red, [240, 150], {0: 20, 1: 100, 2: 300, 3: 750, 4: 925, 5: 1100})
+# gare 3
+ATLANTIC_AVENUE = Property('ATLANTIC_AVENUE', 26, yellow, [260, 150], {0: 22, 1: 110, 2: 330, 3: 800, 4: 975, 5: 1150})
+VENTNOR_AVENUE = Property('VENTNOR_AVENUE', 27, yellow, [260, 150], {0: 22, 1: 110, 2: 330, 3: 800, 4: 975, 5: 1150})
+# water company
+MARVIN_GARDENS = Property('MARVIN_GARDENS', 29, yellow, [280, 150], {0: 24, 1: 120, 2: 360, 3: 850, 4: 1025, 5: 1200})
+GO_TO_JAIL = Special('GO_TO_JAIL', 30, go_to_jail)
+PACIFIC_AVENUE = Property('PACIFIC_AVENUE', 31, green, [300, 200], {0: 26, 1: 130, 2: 390, 3: 900, 4: 1100, 5: 1275})
+NORTH_CAROLINA_AVENUE = Property('NORTH_CAROLINA_AVENUE', 32, green, [300, 200], {0: 26, 1: 130, 2: 390, 3: 900, 4: 1100, 5: 1275})
+COMMUNITY_CHEST3 = Special('COMMUNITY_CHEST', 33, community_chest)
+PENNSYLVANIA_AVENUE = Property('PENNSYLVANIA_AVENUE', 34, green, [320, 200], {0: 28, 1: 150, 2: 450, 3: 1000, 4: 1200, 5: 1400})
+# gare 4
+CHANCE3 = Special('CHANCE', 36, chance)
+PARK_PLACE = Property('PARK_PLACE', 37, darkblue, [350, 200], {0: 35, 1: 175, 2: 500, 3: 1100, 4: 1300, 5: 1500})
+LUXURY_TAX = Special('LUXURY_TAX', 38, tax100)
+BOARDWALK = Property('BOARDWALK', 39, darkblue, [400, 200], {0: 50, 1: 200, 2: 600, 3: 1400, 4: 1700, 5: 2000})
 
-Lcases = []
+
 
 p = Player('player')
